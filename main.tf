26b02f5efa422f4f90a75d32af83936a9ff8a638
David-Lang <16465823+David-Lang@users.noreply.github.com>
2025-01-15 11:43:38 -0800

    update

--- main.tf
+++ main.tf
@@ -14,6 +14,8 @@ variable "api_key" {}
 variable "ssl_cert_path" {}
 #variable "ssl_cert" {}
 
+variable "conjur_secret_id_1" {}
+
 provider "conjur" {
   appliance_url = var.appliance_url
   account = var.account
@@ -23,14 +25,14 @@ provider "conjur" {
   # ssl_cert = var.conjur_ssl_cert
 }
 
-data "conjur_secret" "db_password" {
-  name = "vault9/lob9/safe9/secret9"
+data "conjur_secret" "secret_1" {
+  name = var.conjur_secret_id_1
 }
 
 # data.conjur_secret.db_password.value will be set by the Conjur Provider
-output "db_password_output" {
+output "secret_1_output" {
   //noinspection HILUnresolvedReference
-  value = data.conjur_secret.db_password.value
+  value = data.conjur_secret.secret_1.value
   # Must mark this output as sensitive for Terraform v0.15+,
   # because it's derived from a Conjur variable value that is declared as sensitive.
   sensitive = true
