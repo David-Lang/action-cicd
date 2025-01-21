eaea262430a58e53ced1afebcf32cabeeca97a8d
David-Lang <16465823+David-Lang@users.noreply.github.com>
2025-01-21 14:59:55 -0800

    update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "ssl_cert" {}
 variable "conjur_secret_id_1" {}
 
 
-
+variable "aws_region" { type = string, default = "ca-central-1"  }
 
 # data.conjur_secret.db_password.value will be set by the Conjur Provider
 output "secret_1_output" {
